096d98c15707851e21f2ef64fe7991a63d94f666
Orlando <orlando.dan14@gmail.com>
2018-04-12 16:48:29 -0300

    Sing in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   root 'static_pages#home'
-
   get 'static_pages/home1'
-
   get 'static_pages/home'
-
   get 'static_pages/blog'
-
   get 'static_pages/about'
-
   get 'static_pages/contact'
-
-  get  '/signup',  to: 'users#new'
-  
-  post '/signup',  to: 'users#create'
+  get  	 '/signup',  to: 'users#new'  
+  post 	 '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
 
